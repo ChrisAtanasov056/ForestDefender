966322c5f16f54dce2b876f01fc3b009959a6e85
ChrisAtanasov056 <62712886+ChrisAtanasov056@users.noreply.github.com>
2022-05-27 01:27:16 +0300

    Initial Create, BasicMap, Wave Spawner, Enemy AI

---
+++ Assets/Scripts/WaveSpawn.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawn : MonoBehaviour {
+
+	public GameObject EnemyPrefab;
+
+	public Transform spawnPoint;
+	public Text waveCountdownText;
+	private int waveNumber = 0;
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
+
+
+	void Update()
+	{
+		if (countdown <= 0f )
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+		}
+		countdown -= Time.deltaTime;
+		waveCountdownText.text = Mathf.Round(countdown).ToString();
+	}
+
+	IEnumerator SpawnWave()
+    {
+        waveNumber++;
+		for (int i = 0; i < waveNumber; i++)
+        {
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
+		}
+		
+
+		
+    }
+	void SpawnEnemy()
+	{
+		Instantiate(EnemyPrefab, spawnPoint.position, spawnPoint.rotation);
+      
+    }
+}
