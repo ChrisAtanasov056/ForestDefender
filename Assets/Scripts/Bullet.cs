059aa165d78903faa87ffe5194dcdc6950196ef0
ChrisAtanasov056 <62712886+ChrisAtanasov056@users.noreply.github.com>
2022-06-08 11:29:29 +0300

    Adding new explosion effect and radios damage on big missile

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,10 +1,11 @@
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     private Transform target;
     public float speed = 70f;
-
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
     public void Seek(Transform _target)
     {
@@ -27,6 +28,7 @@ public class Bullet : MonoBehaviour
             return;
         }
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     void HitTarget()
@@ -34,7 +36,37 @@ public class Bullet : MonoBehaviour
         GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effectIns, 2f);
 
-        Destroy(target.gameObject);
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
         Destroy(gameObject);
+    }
+
+     void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider colider in colliders)
+        {
+            if (colider.tag == "Enemy")
+            {
+                Damage(colider.transform);
+            }
+        }
+    }
+
+    void Damage (Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
